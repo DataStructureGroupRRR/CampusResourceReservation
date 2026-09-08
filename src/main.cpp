b4fd007fe0a19644a1dfc922f5c0bd76a29053d5
Rodrigo Martinez <RodrigoMartinez6@my.unt.edu>
2026-09-08 16:24:07 -0500

    outputs all information for each resource

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
 
@@ -6,9 +7,7 @@
 
 using namespace std;
 
-int choice = -1;
-
-void readResources(ifstream& file, vector<Resource>& resources) {
+void readResources(ifstream& file, vector<Resource>& resources, int& longestName, int& longestType) {
     string id;
     string name;
     string type;
@@ -22,16 +21,50 @@ void readResources(ifstream& file, vector<Resource>& resources) {
 
         Resource rs(id, name, type, availableS == "Available");
         resources.push_back(rs);
+
+        // gets the lengths of the longest names for formatting
+        if (name.length() > longestName) {
+            longestName = name.length();
+        }
+
+        if (type.length() > longestType) {
+            longestType = type.length();
+        }
     }
 }
 
-void viewResources(vector<Resource>& resources) {
+void viewResources(vector<Resource>& resources, int longestName, int longestType) {
+    // adds padding to formatting
+
+    int nameLen = max(longestName, 2) + 4;
+    int typeLen = max(longestType, 4) + 4;
+
+    cout << setfill(' ') << left;
+
+    cout << setw(8) << "ID";
+    cout << setw(nameLen) << "Name";
+    cout << setw(typeLen) << "Type";
+    cout << setw(12) << "Availability" << endl;
+
+    cout << setfill('-') << setw(8 + nameLen + typeLen + 12) << "" << endl;
+    
+    cout << setfill(' ');
     for (const Resource& res : resources) {
-        cout << res.GetId() << endl;
+        cout << setw(8) << res.GetId();
+        cout << setw(nameLen) << res.GetName();
+        cout << setw(typeLen) << res.GetType();
+        cout << setw(12) << (res.IsAvailable() ? "Available" : "Unavailable") << endl;
     }
+
+    cout << endl;
 }
 
 int main() {
+    int choice = -1;
+    int longestName = 0;
+    int longestType = 0;
+
+    // reads from resources.txt
     ifstream resourceFile("data/resources.txt");
     vector<Resource> resources;
 
@@ -40,7 +73,7 @@ int main() {
         return 1;
     }
 
-    readResources(resourceFile, resources);
+    readResources(resourceFile, resources, longestName, longestType);
 
     resourceFile.close();
 
@@ -62,7 +95,7 @@ int main() {
 
         switch (choice) {
             case 1:
-                viewResources(resources);
+                viewResources(resources, longestName, longestType);
                 break;
             case 2:
                 break;
