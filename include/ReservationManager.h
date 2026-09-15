c51cee45fdffe4fbaafa91ce24e33120b97c151a
rainthenics <rainlanvel@gmail.com>
2026-09-15 12:16:45 -0500

    implemented reservation cancellation

--- include/ReservationManager.h
+++ include/ReservationManager.h
@@ -22,6 +22,8 @@ class ReservationManager {
 
         Node<Reservation>* head;
         Node<Reservation>* tail;
+
+        std::stack<Reservation> cancelledHistory;
 };
 
 #endif // RESERVATION_MAN_H
--- src/ReservationManager.cpp
+++ src/ReservationManager.cpp
@@ -53,11 +53,70 @@ void ReservationManager::CreateReservation() {
 }
 
 void ReservationManager::CancelReservation() {
+    int id;
 
+    cout << "Reservation ID: ";
+    cin >> id;
+
+    // find the node with the matching reservation ID
+    Node<Reservation>* node = head;
+    while (node) {
+        if (node->value.GetId() == id) {
+            break;
+        }
+
+        node = node->next;
+    }
+
+    if (!node) {
+        cout << "No reservation found with ID " << id << ".\n\n" << flush;
+        return;
+    }
+
+    // unlink node from the doubly linked list
+    if (node->prev) {
+        node->prev->next = node->next;
+    } else {
+        head = node->next;
+    }
+
+    if (node->next) {
+        node->next->prev = node->prev;
+    } else {
+        tail = node->prev;
+    }
+
+    // push the cancelled reservation onto the history stack so it can be undone
+    cancelledHistory.push(node->value);
+    delete node;
+
+    cout << "Reservation Cancelled.\n";
+    cout << "Added to cancellation history.\n\n" << flush;
 }
 
 void ReservationManager::RestoreReservation() {
-    
+    if (cancelledHistory.empty()) {
+        cout << "No cancellations to undo.\n\n" << flush;
+        return;
+    }
+
+    // only the most recently cancelled reservation may be restored
+    Reservation r = cancelledHistory.top();
+    cancelledHistory.pop();
+
+    Node<Reservation>* node = new Node<Reservation>(r);
+
+    if (head) {
+        tail->next = node;
+        node->prev = tail;
+
+        tail = node;
+    } else {
+        head = node;
+        tail = node;
+    }
+
+    cout << "Reservation Restored Successfully.\n\n" << flush;
 }
 
 void ReservationManager::ViewReservations() const {
